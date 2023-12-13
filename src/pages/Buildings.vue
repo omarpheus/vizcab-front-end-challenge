5d81089493c13d3e4335ebe0da2b2f3b313a2af7
Omar Amara <omar.amara@ik.me>
2023-12-13 09:46:50 +0000

    Buildings data is fetched and prerendered

--- src/pages/Buildings.vue
+++ src/pages/Buildings.vue
@@ -1,5 +1,48 @@
+<script setup>
+  import { reactive, onBeforeMount } from 'vue'
+  import SortingVue from '../components/Sorting.vue';
+
+  const buildings = reactive({data: [], key: 0}) // reactive component for the list of buildings
+
+  onBeforeMount(() => {
+    // we fetch data to the backend
+    fetch(
+      `http://localhost:3500`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    .then(async (response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      buildings.data = await response.json()
+      // Now sort the buildings according to their surface or carbon emissions
+      // ...
+    })
+    .catch((error) => {
+      console.error('Error:', error)
+    })
+  })
+
+</script>
+
 <template>
   <div>
-    Buildings page
+    <div><SortingVue /></div>
+    <div :key="buildings.key">
+      <div v-for="building in buildings.data.data" :key="building.id">
+        <div :id="`building-${building.id}`" class="building">
+          <div class="building-name">{{ building.name }}</div>
+          <div class="building-address-street">{{ building.address }}</div>
+          <div class="building-address-postcode-city">
+            <span class="building-address-postcode">{{ building.postcode }}</span> <span class="building-address-city">{{ building.city }}</span>
+          </div>
+          <div class="building-details"><router-link :to="`/buildings/${building.id}`">Details</router-link></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
